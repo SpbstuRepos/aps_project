7b6a660799d01ab2d8cec9804fd80fc393e0ebb9
anton <antkumachev@gmail.com>
2024-12-21 15:42:32 +0300

    Random generator

---
+++ code/utility/random_generator.py
@@ -0,0 +1,25 @@
+from numpy.random import poisson, uniform
+
+
+class RandomGenerator:
+    def generate(self) -> float:
+        raise NotImplementedError()
+
+
+class PoissonGenerator(RandomGenerator):
+    def __init__(self, lam):
+        super().__init__()
+        self._lam = lam
+
+    def generate(self):
+        return float(poisson(self._lam))
+
+
+class EvenGenerator(RandomGenerator):
+    def __init__(self, a, b):
+        super().__init__()
+        self._a = a
+        self._b = b
+
+    def generate(self):
+        return uniform(self._a, self._b)
