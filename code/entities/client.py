6288e5029a9447f931693ba7297612d14ed67d85
anton <antkumachev@gmail.com>
2024-12-21 22:55:40 +0300

    Fix Client

--- code/entities/client.py
+++ code/entities/client.py
@@ -8,10 +8,17 @@ class Client:
         self._id = id
         self._counter = 0
 
+    @property
+    def id(self):
+        return self._id
+
     def create_order(self):
+        return self.create_order(None)
+
+    def create_order(self, status_update_handler):
         self._counter += 1
         order_id = self._counter * 100 + self._id
-        return Order(order_id, self._id, self)
+        return Order(order_id, self._id, self, status_update_handler)
 
     def notify_completed(self, order: Order):
-        pass  # TODO: Handle completed order
+        pass
